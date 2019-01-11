771b9dab1a87745614593344fc3aaae466f11b25
Muhaimenur <Muhaimenur@Tanmoy>
2019-01-11 01:33:48 -0800

    new commit

---
+++ src/pneumonia/diagnosis/PneumoniaDiagnosis.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pneumonia.diagnosis;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author Muhaimenur
+ */
+public class PneumoniaDiagnosis {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Search_patient search_patient = new Search_patient();
+        search_patient.show();
+    }
+    
+}
